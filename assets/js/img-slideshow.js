c78194e417cf24eba7782b820c1ffef7fdfafb4f
Xinyu Hou <u5916376@anu.edu.au>
2020-04-14 14:11:44 +1000

    post

---
+++ assets/js/img-slideshow.js
@@ -0,0 +1,24 @@
+var slideIndex = 1;
+var slides = document.getElementsByClassName("slides");
+
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";  
+  }
+  console.log(slides[slideIndex-1]);
+  slides[slideIndex-1].style.display = "block";  
+}
